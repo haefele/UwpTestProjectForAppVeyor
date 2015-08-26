eab0d5caaabf09ba7f2185e533b7f11ea596e695
Daniel Häfele <haefele@xemio.net>
2015-08-26 19:28:39 +0200

    Well, the last project was no universal project... But this one is...

---
+++ MyPortableClassLibrary/TheProject/FolderAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml;
+
+namespace TheProject
+{
+    public class FolderAdapter
+    {
+        public FolderAdapter(StorageFolder folder)
+        {
+            
+        }
+
+        public void DoThing()
+        {
+            int id = 2;
+            string s = $"the id is {id}";
+        }
+    }
+}
