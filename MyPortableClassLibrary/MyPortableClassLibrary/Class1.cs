b436f31dd79590ee661482b507217a8ec5d0bb84
Daniel Häfele <haefele@xemio.net>
2015-08-26 19:17:55 +0200

    Created the RedditProgrammingClient.

---
+++ MyPortableClassLibrary/MyPortableClassLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPortableClassLibrary
+{
+    public class RedditProgrammingClient
+    {
+        public async Task<List<string>> GetHotThreads()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://www.reddit.com/r/programming/hot/.json?limit=100");
+
+            throw new Exception("Doesn't matter at all!");
+        } 
+    }
+}
